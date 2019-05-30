e8c06aa9b3f541e9b5b8bee0fdc587ac9e2c8084
Michael Tatsuo <mike_tatsuo@hotmail.com>
2019-05-30 03:09:10 -0300

    starting base layout with materialize

--- assets/init.js
+++ assets/init.js
@@ -11,19 +11,19 @@ const changeNavOpacity = () => {
   let actualScroll = window.pageYOffset;
   let navbar = document.getElementById("nav");
   let menuHover = navbar.getElementsByClassName("waves-effect");
-  let porcentagem = actualScroll / parallax[0].offsetHeight;
+  let percentage = actualScroll / parallax[0].offsetHeight;
 
   if (parallax[0].offsetHeight <= actualScroll) {
-    porcentagemm = 1;
+    percentage = 1;
   }
 
-  navbar.style.backgroundColor = `rgba(34, 134, 216, ${porcentagem})`;
+  navbar.style.backgroundColor = `rgba(34, 134, 216, ${percentage})`;
   for (let item of menuHover) {
     item.onmousedown = (e) => {
-      e.target.style.backgroundColor = `rgba(31,85,181,${porcentagem})`;
+      e.target.style.backgroundColor = `rgba(31,85,181,${percentage})`;
     }
     item.onmouseover = (e) => {
-      e.target.style.backgroundColor = `rgba(142,190,226, ${ 0.5 + (porcentagem / 2)})`;
+      e.target.style.backgroundColor = `rgba(142,190,226, ${ 0.5 + (percentage / 2)})`;
     }
     item.onmouseout = (e) => {
       e.target.style.backgroundColor = "rgba(142,190,226, 0)";
